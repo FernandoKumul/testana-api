55bd41b1da2be03845789b3075e0ef81a9e87404
FernandoKumul <ferdesarrollador11@outlook.com>
2024-04-13 18:27:41 -0500

    Update Models and DBContext

--- Data/Models/Question.cs
+++ Data/Models/Question.cs
@@ -26,6 +26,8 @@ public partial class Question
 
     public virtual ICollection<QuestionAnswer> Answers { get; set; } = new List<QuestionAnswer>();
 
+    public virtual ICollection<AnswersQuestionsUser> UserAnswers { get; set; } = new List<AnswersQuestionsUser>();
+
     public virtual QuestionType QuestionType { get; set; } = null!;
 
     [JsonIgnore]
